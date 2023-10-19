830d80d6b6d12632046cfbe30c3fee551dba83b0
devdwr <dawid.jscript@gmail.com>
2023-10-19 15:34:55 +0200

    added set up scripts and running scripts, added databases domain for pizza and user, added readme md file

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+# Running backend
+
+```bash
+cd docker
+chmod +x build_and_start.sh
+./build_and_start.sh
+```
---
+++ backend/src/main/java/com/pwr/edu/backend/domain/pizza/Dough.java
@@ -0,0 +1,7 @@
+package com.pwr.edu.backend.domain.pizza;
+
+public enum Dough {
+    ITALIAN,
+    POLISH,
+    AMERICAN
+}
