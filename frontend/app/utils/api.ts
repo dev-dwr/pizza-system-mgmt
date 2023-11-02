5bb9875e66b532d39e386fd0fe70964544d04da0
Jakub Wiraszka <WirachaPL@wp.pl>
2023-11-02 16:15:51 +0100

    Login Form

---
+++ frontend/app/utils/api.ts
@@ -0,0 +1,48 @@
+import { User } from "./types";
+
+export async function login(user: User): Promise<string> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/v1/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.message);
+  if (data.loggedIn !== "true") throw new Error("Something went wrong!");
+
+  return data.token;
+}
+
+export async function logout(token: string): Promise<void> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/v1/logout`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token }),
+  });
+
+  if (!response.ok) {
+    const data = await response.json();
+    throw new Error(data.message);
+  }
+}
+
+export async function register(user: User): Promise<string> {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER}/api/v1/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+  
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.message);
+    if (data.loggedIn !== "true") throw new Error("Something went wrong!");
+  
+    return data.token;
+  }
